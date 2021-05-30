9f1168e92cc745eeb301f3256890672e3d3d639d
xysun666 <3459264652@qq.com>
2021-05-30 11:07:53 +0800

    second commit

---
+++ src/main/java/com/sxy/pojo/GitTest.java
@@ -0,0 +1,11 @@
+package com.sxy.pojo;
+
+/**
+ * @author xysun
+ * @create 2021-05-30 11:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
