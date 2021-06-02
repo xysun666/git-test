2d3ed9bc388f5f7ebfd6ee26a03e2009fbf2e0fc
VicZyy <319478851@qq.com>
2021-06-02 15:48:06 +0800

    。。。

--- src/main/java/com/sxy/pojo/GitTest.java
+++ src/main/java/com/sxy/pojo/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("Hello Git5!");
         System.out.println("Hello Git6!");
+        System.out.println("Hello Git7!");
     }
 }
